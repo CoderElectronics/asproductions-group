0da52f3fb7d1fb3242ad3ebaea811e8f9e59525f
Ari Stehney <42641932+CoderElectronics@users.noreply.github.com>
2021-02-07 18:32:43 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Arlan Stehney Productions',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'A professional AV Production company based in the Northern Virginia area.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
