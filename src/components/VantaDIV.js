81e1e6daaceedc41fe46522b4c64185df3b9cef4
Ari Stehney <aristehney@gmail.com>
2021-02-11 10:55:11 -0500

    Update VantaDIV React component syntax

--- src/components/VantaDIV.js
+++ src/components/VantaDIV.js
@@ -1,28 +1,39 @@
-import vanta from './vanta.waves.min.js';
+import WAVES from './vanta.waves.min.js';
 import {useRef, useLayoutEffect} from 'react'
-
-export default VantaDIV (props) => {
-    const element = useRef<HTMLDivElement>()
-    useLayoutEffect(() => {
-        vanta({
-            el: element.current,
-            mouseControls: true,
-            touchControls: true,
-            gyroControls: false,
-            minHeight: 200.00,
-            minWidth: 200.00,
-            scale: 1.00,
-            scaleMobile: 1.00,
-            color: 0x8088,
-            shininess: 96.00,
-            waveHeight: 40.00,
-            waveSpeed: 0.35
-        })
+class VantaDIV extends React.Component {
+  constructor() {
+    super()
+    this.vantaRef = React.createRef()
+  }
+  componentDidMount() {
+    this.vantaEffect = BIRDS({
+        el: this.vantaRef.current,
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0x8088,
+        shininess: 96.00,
+        waveHeight: 40.00,
+        waveSpeed: 0.35
     })
+  }
+  componentWillUnmount() {
+    if (this.vantaEffect) this.vantaEffect.destroy()
+  }
+  render() {
     return (
-        <>
-            <script src="/three.min.js"></script>
-            <div id="vantajs" ref={element}></div>
-        </>
+        <div>
+            <script id="vantajs" src="/three.min.js"></script>
+            <div ref={this.vantaRef}>
+            Foreground content goes here
+            </div>
+        </div>
     )
+  }
 }
+
+export default VantaDIV
